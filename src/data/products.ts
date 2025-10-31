b1964d053a09e9b669ceaa03b003d49960d19433
Vinicius Rosan <86744269+krauh1@users.noreply.github.com>
2025-10-30 21:14:00 -0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -16,97 +16,97 @@ export const productCategories: ProductCategory[] = [
       {
         name: '15 Atividades de Desenho',
         price: '12,90',
-        image: 'https://exercitandoocerebro.com/black-images/teste.jpg',
+        image: 'https://exercitandoocerebro.com/black-images/01desenho.jpg',
       },
       {
         name: 'Exercitando a Memória',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=elderly%20person%20playing%20chess',
+          'https://exercitandoocerebro.com/black-images/02memoria.jpg',
       },
       {
         name: '15 Atividades para Atenção',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=person%20focused%20on%20task',
+          'https://exercitandoocerebro.com/black-images/03atencao.jpg',
       },
       {
         name: 'Atividades para Atenção 2',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=student%20studying%20intently',
+          'https://exercitandoocerebro.com/black-images/04atencao.jpg',
       },
       {
         name: '15 Atividades de Criatividade',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=person%20painting%20canvas',
+          'https://exercitandoocerebro.com/black-images/05criatividade.jpg',
       },
       {
         name: '16 Atividades de Pintura',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=hands%20with%20paint%20brushes',
+          'https://exercitandoocerebro.com/black-images/06pintura.jpg',
       },
       {
         name: 'Meu Calendário',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=person%20organizing%20calendar',
+          'https://exercitandoocerebro.com/black-images/07calendario.jpg',
       },
       {
         name: 'Ligando Palavras',
         price: '12,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=people%20playing%20scrabble',
+          'https://exercitandoocerebro.com/black-images/08palavras.jpg',
       },
       {
         name: 'Atividades para Coordenação Motora Fina',
         price: '24,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=child%20threading%20beads',
+          'https://exercitandoocerebro.com/black-images/09coordenacao.jpg',
       },
       {
         name: 'Exercitando o Cérebro',
         price: '29,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=person%20solving%20puzzle',
+          'https://exercitandoocerebro.com/black-images/10cerebro.jpg',
       },
       {
         name: 'Exercitando o Cérebro 2',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=group%20solving%20rubiks%20cube',
+          'https://exercitandoocerebro.com/black-images/11cerebro.jpg',
       },
       {
         name: 'Férias divertidas',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=family%20laughing%20on%20the%20beach',
+          'https://exercitandoocerebro.com/black-images/12ferias.jpg',
       },
       {
         name: 'Dislexia',
         price: '37,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=teacher%20helping%20student%20read',
+          'https://exercitandoocerebro.com/black-images/13dislexia.jpg',
       },
       {
         name: 'TDAH',
         price: '39,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=child%20focused%20on%20building%20blocks',
+          'https://exercitandoocerebro.com/black-images/14tdah.jpg',
       },
       {
         name: 'TDAH 2',
         price: '39,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=therapist%20with%20energetic%20child',
+          'https://exercitandoocerebro.com/black-images/15tdah.jpg',
       },
       {
         name: 'Idosos',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=happy%20elderly%20couple%20gardening',
+          'https://exercitandoocerebro.com/black-images/16idosos.jpg',
       },
     ],
   },
@@ -117,55 +117,55 @@ export const productCategories: ProductCategory[] = [
         name: 'Enfrentando a Ansiedade Social',
         price: '25,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=support%20group%20talking',
+          'https://exercitandoocerebro.com/black-images/17ansiedade.jpg',
       },
       {
         name: 'Baralho Terapêutico para Idosos',
         price: '25,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=seniors%20playing%20cards',
+          'https://exercitandoocerebro.com/black-images/18baralho.jpg',
       },
       {
         name: 'Tabuleiro das Habilidades Sociais',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=children%20playing%20board%20game',
+          'https://exercitandoocerebro.com/black-images/19tabuleiro.jpg',
       },
       {
         name: 'Superando a Depressão Infantil',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=child%20smiling%20with%20therapist',
+          'https://exercitandoocerebro.com/black-images/20depressao.jpg',
       },
       {
         name: 'Tabuleiro da Autoestima',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=person%20looking%20confidently%20in%20mirror',
+          'https://exercitandoocerebro.com/black-images/21autoestima.jpg',
       },
       {
         name: 'Tabuleiro das emoções',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=child%20expressing%20emotions',
+          'https://exercitandoocerebro.com/black-images/22emocoes.jpg',
       },
       {
         name: 'Estilos Parentais',
         price: '34,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=parents%20playing%20with%20child',
+          'https://exercitandoocerebro.com/black-images/23parentais.jpg',
       },
       {
         name: 'Tabuleiro das Distorções Cognitivas',
         price: '37,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=person%20in%20deep%20thought',
+          'https://exercitandoocerebro.com/black-images/24distorcoes.jpg',
       },
       {
         name: 'Ciúme: 40 Recursos Terapêuticos Editáveis',
         price: '49,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=couple%20having%20a%20serious%20conversation',
+          'https://exercitandoocerebro.com/black-images/25ciume.jpg',
       },
     ],
   },
@@ -176,19 +176,19 @@ export const productCategories: ProductCategory[] = [
         name: 'Atividades de Natal',
         price: '19,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=family%20decorating%20christmas%20tree',
+          'https://exercitandoocerebro.com/black-images/26natal.jpg',
       },
       {
         name: 'Atividades de Halloween',
         price: '19,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=kids%20in%20halloween%20costumes',
+          'https://exercitandoocerebro.com/black-images/27halloween.jpg',
       },
       {
         name: 'Atividades de Festa Junina',
         price: '19,90',
         image:
-          'https://img.usecurling.com/p/400/300?q=people%20at%20brazilian%20junina%20party',
+          'https://exercitandoocerebro.com/black-images/28junina.jpg',
       },
     ],
   },
@@ -199,31 +199,31 @@ export const productCategories: ProductCategory[] = [
         name: 'Kit de Atividades Terapêuticas (Raiva, Emoções e Ansiedade)',
         price: '47,00',
         image:
-          'https://img.usecurling.com/p/400/300?q=therapist%20comforting%20patient',
+          'https://exercitandoocerebro.com/black-images/29emocoes.jpg',
       },
       {
         name: 'Kit de Atividades: Seletividade Alimentar',
         price: '65,00',
         image:
-          'https://img.usecurling.com/p/400/300?q=child%20trying%20new%20vegetables',
+          'https://exercitandoocerebro.com/black-images/30seletividade.jpg',
       },
       {
         name: 'Habilidades Sociais para Adolescentes',
         price: '77,00',
         image:
-          'https://img.usecurling.com/p/400/300?q=teenagers%20talking%20and%20laughing',
+          'https://exercitandoocerebro.com/black-images/31habilidades.jpg',
       },
       {
         name: 'Exercitando o Cérebro Autista',
         price: '77,00',
         image:
-          'https://img.usecurling.com/p/400/300?q=autistic%20child%20playing%20with%20sensory%20toys',
+          'https://exercitandoocerebro.com/black-images/32autista.jpg',
       },
       {
         name: 'Crianças Desafiadoras (TOD)',
         price: '77,00',
         image:
-          'https://img.usecurling.com/p/400/300?q=parent%20calming%20down%20a%20frustrated%20child',
+          'https://exercitandoocerebro.com/black-images/33criancas.jpg',
       },
     ],
   },
